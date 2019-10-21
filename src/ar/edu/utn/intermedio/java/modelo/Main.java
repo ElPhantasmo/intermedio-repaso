b437deccb8783e74a15b9b5bd838fdd3ae2fd273
Lucas Cabane <lucas.ecabane@gmail.com>
2019-10-21 20:18:39 -0300

    commit por si se corta la luz

---
+++ src/ar/edu/utn/intermedio/java/modelo/Main.java
@@ -0,0 +1,13 @@
+package ar.edu.utn.intermedio.java.modelo;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		
+		Persona p = new Persona("Nombre", "Apellido");
+		p.identificarse();
+		
+	}
+
+}
