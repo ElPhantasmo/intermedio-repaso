74612c54c4842efd2ae8b7ee4ace6ca8a4ce485a
Señor Bigotes <ema_abitante@hotmail.com>
2019-10-28 21:51:06 -0300

    Carrera

--- src/ar/edu/utn/intermedio/java/ejercicio2/Carrera.java
+++ src/ar/edu/utn/intermedio/java/ejercicio2/Carrera.java
@@ -1,22 +1,51 @@
 package ar.edu.utn.intermedio.java.ejercicio2;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Carrera {
 	
 	
 	public static void main(String[] args) {
 		
+		ArrayList<Moto> listaMotos = new ArrayList<>(); 
 		
 		Moto moto1 = new Moto(2, "", "");
 		moto1.setMarca("Yamaha");
 		moto1.setColor("Amarilla");
+		listaMotos.add(moto1);
 		
 		Moto moto2 = new Moto(2, "", "");
 		moto2.setMarca("Suzuki");
 		moto2.setColor("Roja");
+		listaMotos.add(moto2);
 		
 		Moto moto3 = new Moto(2, "", "");
 		moto3.setMarca("Ducati");
 		moto3.setColor("Negra");
+		listaMotos.add(moto3);
+		
+		
+		Integer minimo = Integer.MAX_VALUE; // listaMotos.get(0).getVelocidadMaxima();
+		Integer velocidad = 0;
+		String maslenta = "";
+		
+		Scanner scanner = new Scanner(System.in);
+		for (Moto moto : listaMotos) {
+			System.out.println("Ingrese velocidad " + moto.getMarca());
+			moto.setVelocidadMaxima(scanner.nextInt());
+			velocidad = moto.avanzar(120);
+			
+			if (velocidad == Math.min(velocidad, minimo)) {
+				minimo = velocidad;
+				maslenta = moto.getMarca();
+		}
+	
+		
+			}
+		
+		System.out.println("Moto mas lenta: " + maslenta);
+		
 	}
 
 }
---
+++ src/ar/edu/utn/intermedio/java/ejercicio2/PruebaMath.java
@@ -0,0 +1,16 @@
+package ar.edu.utn.intermedio.java.ejercicio2;
+
+public class PruebaMath {
+
+	public static void main(String[] args) {
+
+		
+		System.out.println( Math.min(12, 15));
+		
+		System.out.println( Math.max(5, 42));
+		
+		System.out.println( Math.pow(2, 2));
+		
+	}
+
+}
